86c63cd78c6e604f090b66dd7f46444544bab91c
Us10n <srabinin.v@gmail.com>
2022-04-13 14:46:04 +0300

    Small changes

--- web/src/main/java/com/epam/esm/web/controller/TagController.java
+++ web/src/main/java/com/epam/esm/web/controller/TagController.java
@@ -9,16 +9,29 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Tag rest controller.
+ */
 @RestController
 @RequestMapping("/tags")
 public class TagController {
     private TagCRU tagCRU;
 
+    /**
+     * Instantiates a new Tag controller.
+     *
+     * @param tagCRU the tag cru
+     */
     @Autowired
     public TagController(TagCRU tagCRU) {
         this.tagCRU = tagCRU;
     }
 
+    /**
+     * Read all tags.
+     *
+     * @return the list
+     */
     @GetMapping
     @ResponseStatus(HttpStatus.FOUND)
     public List<TagDto> readAllTags() {
@@ -26,6 +39,12 @@ public class TagController {
         return tagList;
     }
 
+    /**
+     * Read tad by id.
+     *
+     * @param id the id
+     * @return the tag dto
+     */
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.FOUND)
     public TagDto readTadById(@PathVariable long id) {
@@ -33,15 +52,27 @@ public class TagController {
         return tagDto;
     }
 
+    /**
+     * Create tag.
+     *
+     * @param tagDto the tag dto
+     * @return the tag dto
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public TagDto createTag(@RequestBody TagDto tagDto) {
         return tagCRU.create(tagDto);
     }
 
+    /**
+     * Delete tag.
+     *
+     * @param id the id
+     * @return the response entity
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<Void> createTag(@PathVariable long id) {
+    public ResponseEntity<Void> deleteTag(@PathVariable long id) {
         tagCRU.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
